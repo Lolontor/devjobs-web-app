4e87ee4ec54b01d2d76f4c8c72f213c515283286
Ayivor-Mawulolo <mawulolo.ayivor@st.vvu.edu.gh>
2023-07-26 14:24:12 +0000

    responsive-pages-and-dark-mode

--- static/js/app.js
+++ static/js/app.js
@@ -1,26 +1,52 @@
-const toggleBtn = document.querySelector('.js_toggle_btn'),
-  body = document.body,
-  cards = document.querySelectorAll('.job_card'),
-  titlebar = document.querySelector('.form');
-  
+const toggleBtn = document.querySelector('.js_toggle_btn');
+const body = document.body;
+const cards = document.querySelectorAll('.job_card');
+const titlebar = document.querySelector('.form');
 
-toggleBtn.addEventListener('click', () => {
-  body.classList.toggle('dark');
+// Function to toggle dark mode
+const toggleDarkMode = () => {
+  const isDarkMode = body.classList.contains('dark');
+  // Toggle the dark mode classes
+  body.classList.toggle('dark', !isDarkMode);
   cards.forEach((card) => {
-    card.classList.toggle('dark');
+    card.classList.toggle('dark', !isDarkMode);
   });
-  titlebar.classList.toggle('dark');
- 
+  titlebar.classList.toggle('dark', !isDarkMode);
+  // Save the dark mode preference to localStorage
+  localStorage.setItem('darkMode', !isDarkMode);
+};
+
+// Event listener for toggling dark mode on button click
+toggleBtn.addEventListener('click', () => {
+  toggleDarkMode();
+  toggleBtn.classList.toggle('active');
 });
 
-toggleBtn.addEventListener('click', () => toggleBtn.classList.toggle('active'));
+// Check and set the initial dark mode state on page load
+document.addEventListener('DOMContentLoaded', () => {
+  const isDarkMode = JSON.parse(localStorage.getItem('darkMode'));
+  if (isDarkMode) {
+    toggleDarkMode();
+    toggleBtn.classList.toggle('active');
+  }
+});
 
 
+const filterBtn = document.querySelector(".js-filter-btn");
+const modal = document.querySelector(".search-modal");
+const overlay = document.querySelector(".overlay");
 
+filterBtn.addEventListener('click', displayModal);
 
+// display modal function 
+function displayModal() {
+  console.log('hello modal');
+  overlay.classList.add('show');
+  modal.classList.remove('hidden');
 
+};
 
-
-
-
-
+overlay.addEventListener('click', () => {
+  overlay.classList.remove('show');
+  modal.classList.add('hidden');
+});
--- static/js/content.js
+++ static/js/content.js
@@ -1,20 +1,38 @@
 const toggleBtn = document.querySelector('.js_toggle_btn'),
   company_detail = document.querySelector('.company_details'),
   job_requirement = document.querySelector('.job_requirement'),
+  company_info = document.querySelector('.company_info'),
   body = document.body,
   h3 = document.querySelectorAll('h3'),
   footer = document.querySelector('footer');
 
-toggleBtn.addEventListener('click', () => {
-  company_detail.classList.toggle('dark');
-  job_requirement.classList.toggle('dark');
-  footer.classList.toggle('dark');
-  body.classList.toggle('dark');
+// toggle dark mode function
+const toggleDarkMode = () => {
+  const isDarkMode = body.classList.contains('dark');
+  // Toggle the dark mode classes
+  company_detail.classList.toggle('dark', !isDarkMode);
+  job_requirement.classList.toggle('dark', !isDarkMode);
+  footer.classList.toggle('dark', !isDarkMode);
+  body.classList.toggle('dark', !isDarkMode);
+  company_info.classList.toggle('dark', !isDarkMode);
   h3.forEach((h) => {
-    h.classList.toggle('dark');
+    h.classList.toggle('dark', !isDarkMode);
   });
-  console.log('hello');
+  // saving dark mode preference to localStorage
+  localStorage.setItem('darkMode', !isDarkMode);
+};
 
+// Event listener for toggling dark mode on button click
+toggleBtn.addEventListener('click', () => {
+  toggleDarkMode();
+  toggleBtn.classList.toggle('active');
 });
 
-toggleBtn.addEventListener('click', () => toggleBtn.classList.toggle('active'));
+// Check and set the initial dark mode state on page load
+document.addEventListener('DOMContentLoaded', () => {
+  const isDarkMode = JSON.parse(localStorage.getItem('darkMode'));
+  if (isDarkMode) {
+    toggleDarkMode();
+    toggleBtn.classList.toggle('active');
+  }
+});
